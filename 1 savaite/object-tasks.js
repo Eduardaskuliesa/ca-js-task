39bb5fb775f3948c00cf304dc1515c414411defb
Eduardaskuliesa <eduardas2000@mail.ru>
2022-11-20 19:12:21 +0200

    feat(78% dabaigta kito nepavyko)

--- 1 savaite/object-tasks.js
+++ 1 savaite/object-tasks.js
@@ -1,25 +1,51 @@
 console.groupCollapsed('1 - https://edabit.com/challenge/nuXdWHAoHv9y38sn7');
 {
-
+    const drinks = [
+        {name: 'lemonade', price: 90}, 
+        {name: 'lime', price: 432}, 
+        {name: 'peach', price: 23}
+    ];
+    function sortDrinkByPrice(drinks) {
+        drinks.sort((a,b) => {
+           return(a.price > b.price) ? 1 : -1
+       })
+       return drinks
+   }
+   console.log(sortDrinkByPrice(drinks));
 }
 console.groupEnd();
 
 console.groupCollapsed('2 - https://edabit.com/challenge/9KEKJG5PZTFmG3Zau');
 {
-
+    function addName(obj, name, value) {
+        obj[name] = value;
+        return obj;
+    }
 }
 console.groupEnd();
 
 console.groupCollapsed('3 - https://edabit.com/challenge/48EJWLhF224na8po3');
 {
+    
 
 }
 console.groupEnd();
 
 console.groupCollapsed('4 - https://edabit.com/challenge/i6YqzHcSiPiEQKjeX');
 {
-
+    let maximumScore = ([
+        { tile: "N", score: 1 },
+        { tile: "K", score: 5 },
+        { tile: "Z", score: 10 },
+        { tile: "X", score: 8 },
+        { tile: "D", score: 2 },
+        { tile: "A", score: 1 },
+        { tile: "E", score: 1 }
+    ]) 
+    let score = maximumScore.{score};
+    console.log(score)
 }
+
 console.groupEnd();
 
 console.groupCollapsed('5 - https://edabit.com/challenge/8s2jy9hR2TAeQinKD');
