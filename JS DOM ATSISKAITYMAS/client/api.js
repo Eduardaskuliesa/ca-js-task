1b985f1a46d54214788966bffe377ffdc9e5d747
Eduardaskuliesa <eduardas2000@mail.ru>
2022-12-31 23:11:26 +0200

    feat(created method Delet items with full functionality wich deletes item form server and web)

--- JS DOM ATSISKAITYMAS/client/api.js
+++ JS DOM ATSISKAITYMAS/client/api.js
@@ -8,6 +8,19 @@ const API = {
         } catch(error){
           return error;
         }
-    }
-}
+      },
+      async deletItem(id){
+        try {
+          const respons = await fetch(`http://localhost:5000/items/${id}`,{
+            method: 'DELETE',
+          }) ;
+          const deletedItem = await respons.json();
+ 
+           return deletedItem;
+         } catch(error){
+           return error;
+         }
+
+      }
+};
 export default API
--- JS DOM ATSISKAITYMAS/client/components/concrete/items-table-component.js
+++ JS DOM ATSISKAITYMAS/client/components/concrete/items-table-component.js
@@ -1,3 +1,5 @@
+import API from "../../api.js";
+
 class ItemsTableComponent {
     htmlElement;
 
@@ -5,26 +7,44 @@ class ItemsTableComponent {
         this.htmlElement = document.createElement('table')
         this.htmlElement.className = 'table table-striped shadow'
         this.htmlElement.innerHTML = `
-        <table class="">
         <thead class="bg-dark text-white">
           <tr>
+            <th>ID</th>
             <th>Brand</th>
             <th>Module</th>
             <th>InStock</th>
             <th>Price in USD</th>
+            <th>Actions</th>
           </tr>
         </thead>
-        <tbody>
-        ${items.map(({brand, module, inStock, price}) => `
-        <tr>
-          <td>${brand}</td>
-          <td>${module}</td>
-          <td>${inStock ? 'Yes' : 'No'}</td>
-          <td>${price}</td>
-        </tr>
-        `).join('')}
-        </tbody>
-      </table>`
+        <tbody></tbody>`;
+
+       const tbody = this.htmlElement.querySelector('tbody');
+       const rowsHtmlElements = items.map(this.createRow);
+       tbody.append(...rowsHtmlElements);
+    }
+
+
+    createRow({id, brand, module, inStock, price}){
+      const tr = document.createElement('tr');
+      tr.innerHTML= `
+        <td>${id}</td>
+        <td>${brand}</td>
+        <td>${module}</td>
+        <td>${inStock ? 'Yes' : 'No'}</td>
+        <td>${price}</td>
+        <td class= "d-flex justify-content-end">
+          <button class= "btn btn-danger">X</button>     
+        </td>`;
+
+
+      const delButton = tr.querySelector('.btn-danger');
+      delButton.addEventListener('click', async () => {
+        await API.deletItem(id);
+        const items = await API.getItems();
+      })
+      
+      return tr;   
     }
 }
 export default ItemsTableComponent
