c06be4df82f27609493e69258619dc1d9286a916
Eduardaskuliesa <eduardas2000@mail.ru>
2023-01-01 23:55:12 +0200

    feat(add sumbit event listener to TodoForm)

--- JS DOM ATSISKAITYMAS/client/components/concrete/todo-form-components.js
+++ JS DOM ATSISKAITYMAS/client/components/concrete/todo-form-components.js
@@ -7,14 +7,28 @@ class TodoFormComponents {
         this.htmlElement.innerHTML = `
         <h2 class="h2 text-center">Create Todo</h2>
         <div class="mb-3">
-          <label for="todo-title" class="form-label">Title</label>
-          <input type="email" class="form-control" id="todo-title" aria-describedby="emailHelp">
+          <label for="todo-car" class="form-label">Car brand</label>
+          <input type="text" class="form-control" id="todo-car" name ="carBrand">
+        </div>  
         <div class="mb-3 form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1">
-          <label class="form-check-label" for="exampleCheck1">Check me out</label>
+          <input type="checkbox" class="form-check-input" id="todo-done" name ="done">
+          <label class="form-check-label" for="todo-done">Add this car in market place</label>
         </div>
-        <button type="submit" class="btn btn-primary w-100">Submit</button>  
-        `
+        <button type="submit" class="btn btn-primary w-100">Yeah</button>`;
+
+        this.htmlElement.addEventListener('submit' , (event) => {
+            event.preventDefault();
+
+            const formData = new FormData(event.target );
+            const carBrand = formData.get('carBrand');
+            const done = formData.get('done') === 'on';
+
+            const dataForTodoCreation = {carBrand, done};
+                
+
+            console.log(dataForTodoCreation);
+        })
+
     }
 }
 export default TodoFormComponents
