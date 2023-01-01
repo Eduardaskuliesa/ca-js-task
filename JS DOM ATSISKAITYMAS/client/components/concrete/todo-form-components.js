4d2ffec90f0d7269657d9fac33862f99ec4b83c4
Eduardaskuliesa <eduardas2000@mail.ru>
2023-01-02 00:18:18 +0200

    feat(add more todo-form options like car - [ price and module])

--- JS DOM ATSISKAITYMAS/client/components/concrete/todo-form-components.js
+++ JS DOM ATSISKAITYMAS/client/components/concrete/todo-form-components.js
@@ -7,12 +7,20 @@ class TodoFormComponents {
         this.htmlElement.innerHTML = `
         <h2 class="h2 text-center">Create Todo</h2>
         <div class="mb-3">
-          <label for="todo-car" class="form-label">Car brand</label>
-          <input type="text" class="form-control" id="todo-car" name ="carBrand">
+          <label for="todo-brand" class="form-label">Car brand</label>
+          <input type="text" class="form-control" id="todo-brand" name ="brand">
+        </div>
+        <div class="mb-3">
+          <label for="todo-module" class="form-label">Car module</label>
+          <input type="text" class="form-control" id="todo-module" name ="module">
+        </div> 
+        <div class="mb-3">
+        <label for="todo-price" class="form-label">Car price in USD</label>
+        <input type="text" class="form-control" id="todo-price" name ="price">
         </div>  
         <div class="mb-3 form-check">
-          <input type="checkbox" class="form-check-input" id="todo-done" name ="done">
-          <label class="form-check-label" for="todo-done">Add this car in market place</label>
+          <input type="checkbox" class="form-check-input" id="todo-instock" name ="instock">
+          <label class="form-check-label" for="todo-instock">Add this car in market place</label>
         </div>
         <button type="submit" class="btn btn-primary w-100">Yeah</button>`;
 
@@ -20,10 +28,12 @@ class TodoFormComponents {
             event.preventDefault();
 
             const formData = new FormData(event.target );
-            const carBrand = formData.get('carBrand');
-            const done = formData.get('done') === 'on';
+            const brand = formData.get('brand');
+            const module = formData.get('module');
+            const price = formData.get('price');
+            const instock = formData.get('instock') === 'on';
 
-            const dataForTodoCreation = {carBrand, done};
+            const dataForTodoCreation = {brand, instock, module, price}
                 
 
             console.log(dataForTodoCreation);
